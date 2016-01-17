c1f9c916c4082b9dd02730a519fb8abb45fbedf1
Justin Willis <jgw9617@gmail.com>
2016-01-17 00:47:43 -0600

    formatting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import functions
 def main():
 
     arguments = sys.argv
+    
     try:
         if arguments[1] == "--read":
             functions.read_file(arguments[2])
@@ -16,7 +17,7 @@ def main():
             print("To read a file use --read followed by a file name, to write a new file use --write followed by a file name, and to edit a file already exisiting use --append followed by the file name.")
     except:
         print("You must supply at'least one argument, for help use --help")
-        
+
 
 
 main()
